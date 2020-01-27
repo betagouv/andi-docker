8d32b2da4fd9d400b199f695696628e84f6b8b4f
pmontens <pieterjan@montens.net>
2020-01-27 15:31:30 +0100

    node serve unusable, switching to dev serving

--- evolutility/start.sh
+++ evolutility/start.sh
@@ -2,6 +2,6 @@
 echo -e "DANGEROUSLY_DISABLE_HOST_CHECK=true\n" > /src/ui/.env.development.local
 pm2 start ./server/bin/www
 cd ./ui
-ls -lah
-serve -s -S --debug build -l 3000
-# npm start
+# Unable to serve build folder
+# serve -s -S --debug build -l 3000
+npm start
