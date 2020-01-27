5c1f26f836b16374d37ec392ebbfb060bb54fd1e
pmontens <pieterjan@montens.net>
2020-01-27 14:53:47 +0100

    npm deploy testing

--- evolutility/start.sh
+++ evolutility/start.sh
@@ -3,5 +3,5 @@ echo -e "DANGEROUSLY_DISABLE_HOST_CHECK=true\n" > /src/ui/.env.development.local
 pm2 start ./server/bin/www
 cd ./ui
 ls -lah
-serve -s build -l 3000
+serve -s -S --debug build -l 3000
 # npm start
