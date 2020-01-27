fd03ae2775fa7c8742d649e96c3eda526837dc18
pmontens <pieterjan@montens.net>
2020-01-27 15:52:04 +0100

    found node serving bug, fixed

--- evolutility/start.sh
+++ evolutility/start.sh
@@ -2,6 +2,4 @@
 echo -e "DANGEROUSLY_DISABLE_HOST_CHECK=true\n" > /src/ui/.env.development.local
 pm2 start ./server/bin/www
 cd ./ui
-# Unable to serve build folder
-# serve -s -S --debug build -l 3000
-npm start
+serve -s build -l 3000
